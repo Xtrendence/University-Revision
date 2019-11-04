3eed8f68bc99e80aae4c35d6f0749637bba0e9ee
Xtrendence <xtrendence@gmail.com>
2019-11-04 10:10:38 +0000

    Update.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		var link_elements = document.getElementsByTagName("link");
 		for(i = 0; i < link_elements.length; i++) {
 			if(link_elements[i].getAttribute("rel") == "stylesheet") {
-				link_elements[i].href = link_elements[i].href + "?10";
+				link_elements[i].href = link_elements[i].href + "?11";
 			}
 		}
 		document.getElementsByClassName("js-file")[0].src = document.getElementsByClassName("js-file")[0].src + "?" + epoch();
 	}
-	if(document.getElementsByClassName("card").length == 0) {
+	if(document.getElementsByClassName("card").length == 0 && document.getElementsByClassName("cards-wrapper").length > 0) {
 		document.getElementsByClassName("card-divider")[0].textContent = "No Notes Found";
 	}
 	// Get the current UNIX timestamp.
