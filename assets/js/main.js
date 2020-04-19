a6e9b6fd4148328ec167e09149d78f88869ea757
Xtrendence <xtrendence@yahoo.com>
2020-04-19 18:38:43 +0100

    Added search functionality.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(e) {
 		var link_elements = document.getElementsByTagName("link");
 		for(i = 0; i < link_elements.length; i++) {
 			if(link_elements[i].getAttribute("rel") == "stylesheet") {
-				link_elements[i].href = link_elements[i].href + "?14";
+				link_elements[i].href = link_elements[i].href + "?15";
 			}
 		}
 		document.getElementsByClassName("js-file")[0].src = document.getElementsByClassName("js-file")[0].src + "?" + epoch();
@@ -27,6 +27,49 @@ document.addEventListener("DOMContentLoaded", function(e) {
 				}
 			}
 		});
+
+		document.getElementsByClassName("notes-search")[0].addEventListener("keydown", function() {
+			filter(document.getElementsByClassName("notes-search")[0].value);
+		});
+		document.getElementsByClassName("notes-search")[0].addEventListener("keyup", function() {
+			filter(document.getElementsByClassName("notes-search")[0].value);
+		});
+
+		function filter(text) {
+			var noteContainers = document.getElementsByClassName("notes-container");
+			var noteTitles = document.getElementsByClassName("notes-title");
+			var noteDescriptions = document.getElementsByClassName("notes-description");
+
+			for(var i = 0; i < noteContainers.length; i++) {
+				noteContainers[i].classList.remove("match");
+			}
+
+			var text = text.trim().toLowerCase();
+
+			if(text === "") {
+				for(var i = 0; i < noteContainers.length; i++) {
+					noteContainers[i].classList.remove("hidden");
+					noteContainers[i].classList.remove("match");
+				}
+			}
+			else {
+				for(var i = 0; i < noteTitles.length; i++) {
+					if(noteTitles[i].textContent.toLowerCase().includes(text)) {
+						noteTitles[i].parentElement.classList.add("match");
+					}
+				}
+				for(var i = 0; i < noteDescriptions.length; i++) {
+					if(noteDescriptions[i].textContent.toLowerCase().includes(text)) {
+						noteDescriptions[i].parentElement.classList.add("match");
+					}
+				}
+				for(var i = 0; i < noteContainers.length; i++) {
+					if(!noteContainers[i].classList.contains("match")) {
+						noteContainers[i].classList.add("hidden");
+					}
+				}
+			}
+		}
 	}
 	// Get the current UNIX timestamp.
 	function epoch() {
